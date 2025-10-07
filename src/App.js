96116d61a079654f5a839eab3d3be21ac538c390
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-07 12:40:56 +0430

    CSS and some structure code added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home"
+import About from "./pages/about"
+import Skills from "./pages/skills"
+import Resume from "./pages/resume"
+import Portfolio from "./pages/portfolio"
+import Contact from "./pages/contact"
 
 const App = () => {
-  return <div>App</div>
+  // Particles js
+  // NavBar
+  // main page content
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  )
 }
 
 export default App
