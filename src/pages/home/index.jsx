30eaf549962851cac054437f97de4f0099b22da7
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-11 13:19:44 +0430

    Added button for the contact page and also we should add this packages for particles to work "npm install @tsparticles/react @tsparticles/engine"

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,13 @@
 import React from "react"
+import { Animate } from "react-simple-animate"
 import "./style.scss"
+import { useNavigate } from "react-router-dom"
 
 const Index = () => {
+  const navigate = useNavigate()
+  const handleClickHireMe = () => {
+    navigate("/contact")
+  }
   return (
     <section id="home" className="home">
       <div className="home__text-wrapper">
@@ -9,6 +15,17 @@ const Index = () => {
           Hello, I'm Waheedullah Azizi <br /> Fullstack Developer
         </h1>
       </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{ transform: "translateY(550px)" }}
+        end={{ transform: "translateX(0px)" }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleClickHireMe}>Hire Me</button>
+        </div>
+      </Animate>
     </section>
   )
 }
