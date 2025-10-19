f0bee9bcd861fbc453af6fac29f9fc55ba7f56ff
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-19 11:40:44 +0430

    add skills page content

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,6 +1,9 @@
 import { BsInfoCircleFill } from "react-icons/bs"
 import PageHeaderContent from "../../components/PageHeader"
-
+import { Animate, AnimateKeyframes } from "react-simple-animate"
+import "./style.scss"
+import { skillsData } from "./utils"
+import { Line } from "rc-progress"
 const Index = () => {
   return (
     <section id="skills" className="skills">
@@ -8,6 +11,44 @@ const Index = () => {
         headerText="My Skills"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="skills__content-wrapper">
+        {skillsData.map((item, i) => (
+          <div key={i} className="skills__content-wrapper__inner-content">
+            <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{ transform: "translateX(-200px)" }}
+              end={{ transform: "translateX(0px)" }}
+            >
+              <h3 className="skills__content-wrapper__inner-content__category-text">
+                {item.label}
+                <div>
+                  {item.data.map((skillsItem, index) => (
+                    <AnimateKeyframes
+                      play
+                      duration={1}
+                      keyframes={["opacity:1", "opacity:0"]}
+                      iterationCount="1"
+                    >
+                      <div className="progressbar-wrapper" key={index}>
+                        <p>{skillsItem.skillName}</p>
+                        <Line
+                          percent={skillsItem.percentage}
+                          strokeWidth="2"
+                          strokeColor="var(--yellow-theme-main-color)"
+                          trailWidth="2"
+                          strokeLinecap="square"
+                        />
+                      </div>
+                    </AnimateKeyframes>
+                  ))}
+                </div>
+              </h3>
+            </Animate>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
---
+++ src/pages/skills/utils.js
@@ -0,0 +1,55 @@
+import { data } from "react-router-dom"
+
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "90",
+      },
+      {
+        skillName: "CSS",
+        percentage: "50",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "JAVA",
+        percentage: "90",
+      },
+      {
+        skillName: "PYTHON",
+        percentage: "50",
+      },
+      {
+        skillName: "C#",
+        percentage: "80",
+      },
+    ],
+  },
+  {
+    label: "DATABASE",
+    data: [
+      {
+        skillName: "MYSQL",
+        percentage: "90",
+      },
+      {
+        skillName: "MONGODB",
+        percentage: "50",
+      },
+      {
+        skillName: "ORACLE",
+        percentage: "80",
+      },
+    ],
+  },
+]
