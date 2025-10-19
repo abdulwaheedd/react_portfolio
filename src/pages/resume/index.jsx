bbdd19a08a05025f2c4f5f3d9692cd6a8d94484a
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-19 15:32:55 +0430

    react-vertical-timeline-component issue solved

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,5 +1,10 @@
 import { BsInfoCircleFill } from "react-icons/bs"
 import PageHeaderContent from "../../components/PageHeader"
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import { data } from "./utils"
 const Index = () => {
   return (
     <section id="resume" className="resume">
@@ -7,6 +12,25 @@ const Index = () => {
         headerText="Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout="1-column"
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {/* {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+              ></VerticalTimelineElement>
+            ))} */}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+        </div>
+      </div>
     </section>
   )
 }
---
+++ src/pages/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+  experience: [
+    {
+      title: "Consultant",
+      subTitle: " PwC Afghanistan",
+      description:
+        " Worked as a consultant for various clients, providing strategic advice and solutions to improve their business operations.",
+    },
+    {
+      title: "Senior Consultant",
+      subTitle: " MoPH Afghanistan",
+      description:
+        " Led a team of consultants to implement health sector reforms and improve healthcare delivery across the country.",
+    },
+    {
+      title: "MIS Specialist",
+      subTitle: " MoEC Afghanistan",
+      description:
+        " Developed and managed the Management Information System (MIS) to enhance data collection and reporting for educational institutions.",
+    },
+  ],
+  education: [
+    {
+      title: "High School",
+      subTitle: " Zarghoon Shaher High School, Afghanistan",
+      description:
+        "12th Grade Completed with Science Major, Graduated with High Honors.",
+    },
+    {
+      title: "University",
+      subTitle: "Kabul University, Afghanistan",
+      description:
+        "Bachelor of Science in Computer Science, Graduated with Distinction.",
+    },
+    {
+      title: "Master's Degree",
+      subTitle: "Kabul University, Afghanistan",
+      description:
+        "Master of Computer Science (MCS), Specialized in Information Technology Management.",
+    },
+  ],
+}
