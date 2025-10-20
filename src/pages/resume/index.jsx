35a841bc8a5fa64875d69c9d9bdba13089e2db0c
Waheedullah Azizi <abdulwaheeddanishjo@gmail.com>
2025-10-20 12:56:14 +0430

    resume page completed

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -4,7 +4,10 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component"
+import { MdWork } from "react-icons/md"
 import { data } from "./utils"
+import "react-vertical-timeline-component/style.min.css"
+import "./style.scss"
 const Index = () => {
   return (
     <section id="resume" className="resume">
@@ -19,16 +22,65 @@ const Index = () => {
             layout="1-column"
             lineColor="var(--yellow-theme-main-color)"
           >
-            {/* {data.experience.map((item, i) => (
+            {data.experience.map((item, i) => (
               <VerticalTimelineElement
                 key={i}
                 className="timeline__experience__vertical-timeline-element"
-              ></VerticalTimelineElement>
-            ))} */}
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date="2020 - Present"
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "var(--yellow-theme-main-color)",
+                  color: "#181818",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
           </VerticalTimeline>
         </div>
         <div className="timeline__education">
           <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout="1-column"
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+                contentStyle={{
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
+                }}
+                date="2020 - Present"
+                icon={<MdWork />}
+                iconStyle={{
+                  background: "var(--yellow-theme-main-color)",
+                  color: "#181818",
+                }}
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
+                </div>
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
         </div>
       </div>
     </section>
